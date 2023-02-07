e7a4a16d3c13c845444eff2a393f4b0f5b388bd8
Chirayu Jhakal <chirayu.jhakal.ug20@nsut.ac.in>
2023-02-08 01:28:58 +0530

    MongoDB Connected and Seeder Bug

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+mongoose.set('strictQuery', true);
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
