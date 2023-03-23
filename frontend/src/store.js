207ffd51bb92e521b52fd9f3a0a683db590ef71c
Chirayu Jhakal <jhakalchirayu@gmail.com>
2023-03-23 21:33:48 +0530

    Complete Checkout Till payment method

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,11 @@ import {
   userUpdateProfileReducer,
 } from './reducers/userReducers';
 import { cartReducer } from './reducers/cartReducers';
-import { orderCreateReducer } from './reducers/orderReducers';
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+} from './reducers/orderReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -23,6 +27,8 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
